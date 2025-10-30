1b9010cd96c7952a23098f8c3368271428a81ae7
Moshe <moshe@example.com>
2025-10-30 12:14:08 +0200

    player_io.py clean and clear

--- core/player_io.py
+++ core/player_io.py
@@ -1,6 +1,9 @@
+
+
 def ask_player_action() -> str:
     while True:
-        user_input = input('please enter H or S').strip()
+        user_input = input('please enter H or S\n').strip()
         if user_input == 'S' or user_input == 'H':
             return user_input
+        print('you can only enter H or S')
 
